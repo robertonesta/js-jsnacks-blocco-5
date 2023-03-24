06e16c78524dbdfbb7ebf9d92ee9cb295394ec9c
RobertoNesta <robertonesta1993@gmail.com>
2023-03-24 12:09:41 +0100

    adding array

--- snack1/assets/js/main.js
+++ snack1/assets/js/main.js
@@ -1,3 +1,57 @@
 /*Snack 1:
 Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine.*/
 
+//array 
+
+const zucchini = [
+    {
+        type: "zucchini 1",
+        weigth: "300gr",
+        length: "46cm"
+    },
+    {
+        type: "zucchini 2",
+        weigth: "320gr",
+        length: "48cm"
+    },
+    {
+        type: "zucchini 3",
+        weigth: "340gr",
+        length: "50cm"
+    },
+    {
+        type: "zucchini 4",
+        weigth: "360gr",
+        length: "52cm"
+    },
+    {
+        type: "zucchini 5",
+        weigth: "380gr",
+        length: "54cm"
+    },
+    {
+        type: "zucchini 6",
+        weigth: "400gr",
+        length: "56cm"
+    },
+    {
+        type: "zucchini 7",
+        weigth: "420gr",
+        length: "58cm"
+    },
+    {
+        type: "zucchini 8",
+        weigth: "440gr",
+        length: "60cm"
+    },
+    {
+        type: "zucchini 9",
+        weigth: "460gr",
+        length: "62cm"
+    },
+    {
+        type: "zucchini 10",
+        weigth: "480gr",
+        length: "64cm"
+    },
+]
