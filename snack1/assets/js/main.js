ef647769d307b24f346c7a50ab58e9dba80b0116
RobertoNesta <robertonesta1993@gmail.com>
2023-03-24 12:20:44 +0100

    adding the total weigth

--- snack1/assets/js/main.js
+++ snack1/assets/js/main.js
@@ -6,52 +6,62 @@ Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna
 const zucchini = [
     {
         type: "zucchini 1",
-        weigth: "300gr",
-        length: "46cm"
+        weigth: 300,
+        length: 46
     },
     {
         type: "zucchini 2",
-        weigth: "320gr",
-        length: "48cm"
+        weigth: 320,
+        length: 48
     },
     {
         type: "zucchini 3",
-        weigth: "340gr",
-        length: "50cm"
+        weigth: 340,
+        length: 50
     },
     {
         type: "zucchini 4",
-        weigth: "360gr",
-        length: "52cm"
+        weigth: 360,
+        length: 52
     },
     {
         type: "zucchini 5",
-        weigth: "380gr",
-        length: "54cm"
+        weigth: 380,
+        length: 54
     },
     {
         type: "zucchini 6",
-        weigth: "400gr",
-        length: "56cm"
+        weigth: 400,
+        length: 56
     },
     {
         type: "zucchini 7",
-        weigth: "420gr",
-        length: "58cm"
+        weigth: 420,
+        length: 58
     },
     {
         type: "zucchini 8",
-        weigth: "440gr",
-        length: "60cm"
+        weigth: 440,
+        length: 60
     },
     {
         type: "zucchini 9",
-        weigth: "460gr",
-        length: "62cm"
+        weigth: 460,
+        length: 62
     },
     {
         type: "zucchini 10",
-        weigth: "480gr",
-        length: "64cm"
+        weigth: 480,
+        length: 64
     },
 ]
+
+let sum = 0
+
+zucchini.forEach(zucchina => {
+    sum +=zucchina.weigth;
+});
+
+//console.log(`the total weigth is ${sum} grams`)
+const kg = sum / 1000;
+console.log(`the total weigth is ${kg} kg`)
